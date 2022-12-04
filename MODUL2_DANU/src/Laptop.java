01e5307ec4b18b46e196cd6c74c2ae0c7979af66
danuwardanaaa <115786433+danuwardanaaa@users.noreply.github.com>
2022-12-04 08:57:24 +0700

    push modul 2

---
+++ MODUL2_DANU/src/Laptop.java
@@ -0,0 +1,29 @@
+public class Laptop extends Perangkat{
+    protected boolean webcam;
+
+    public Laptop(String drive, int ram, float processor, boolean webcam){
+        super(drive, ram, processor);
+        this.webcam = webcam;
+    }
+
+    @Override
+    public void informasi(){
+        if(webcam){
+            System.out.println("laptop ini memiliki drive tipe " + this.drive + " dengan RAM sebesar "+ this.ram + "GB dan processor secepat " + this.processor + "Ghz. Selain itu laptop ini memiliki webcam");
+        }else{
+            System.out.println("laptop ini memiliki drive tipe " + this.drive + " dengan RAM sebesar "+ this.ram + "GB dan processor secepat " + this.processor + "Ghz. Selain itu laptop ini TIDAK memiliki webcam");
+        }
+    }
+
+    public void bukaGame(String nama_game){
+        System.out.println("Laptop berhasil membuka game " + nama_game);
+    }
+
+    public void kirimEmail(String email){
+        System.out.println("Laptop berhasil mengirim Email ke " + email);
+    }
+
+    public void kirimEmail(String email1, String email2){
+        System.out.println("Laptop berhasil mengirim Email ke " + email1 + " dan ke " + email2);
+    }
+}
