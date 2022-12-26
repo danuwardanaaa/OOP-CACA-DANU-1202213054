f8dec709363aebfc560511388cd9e39bc72e14bd
danuwardanaaa <115786433+danuwardanaaa@users.noreply.github.com>
2022-12-26 12:09:49 +0700

    MODUL 3

---
+++ MODUL3_DANU/src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+        do{
+            try {
+                switch(){
+                    case 1 :
+
+                    case 2 :
+                    case 3 :
+                    case 0 :
+
+                    default :
+
+
+
+
+                }
+                repeat = 12;
+
+
+            } catch (Exception e) {
+                // TODO: handle exception
+            }catch (Exception e) {
+                // TODO: handle exception
+            }catch (Exception e) {
+                // TODO: handle exception
+            }
+        } while(){
+
+        }
+    }
+}
